2aed50fe530d7c50b11e37c20e1b1f726a11eb61
Lrd134 <larryc3200@gmail.com>
2021-08-11 18:03:45 -0400

    handled omniauth_data

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   def create
-    
+    session[:omniauth_data] = request.env['omniauth.auth']
+    redirect_to :new_user_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post] 
+  match '/auth/:provider/callback', to: 'session#create', via: [:get, :post]
+   
   root 'application#home'
 end
