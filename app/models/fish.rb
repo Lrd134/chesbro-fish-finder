ab9231674150c611ba9723baa4afd138afc59d50
Lrd134 <larryc3200@gmail.com>
2021-08-14 15:46:29 -0400

    added callbacks and validations to fish

--- app/models/fish.rb
+++ app/models/fish.rb
@@ -1,3 +1,27 @@
 class Fish < ApplicationRecord
 
+  after_validation :capitalize_title, :update_category
+  before_create :force_unsolved
+  validates :title, presence: true
+
+  def slug
+    self.title = self.title.split(" ").map { | word | word.downcase }.join("-")
+  end
+
+  def deslugify(slug)
+    slug.split("-").map { | word | word.capitalize }.join(" ")
+  end
+
+  def capitalize_title
+    self.title = self.title.split(" ").map { | word | word.capitalize }.join(" ")
+  end
+
+  def update_category
+    self.solved == 1 ? self.category_id = 1 : self.category_id = 2
+  end
+  def force_unsolved
+    self.solved = 0
+    self.category_id = 2
+  end
+
 end
