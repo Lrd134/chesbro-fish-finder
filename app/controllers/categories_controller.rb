db562340a916ec9f8aa484f0779a9a1e0f618bed
Lrd134 <larryc3200@gmail.com>
2021-08-14 18:11:33 -0400

    fixed categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,10 @@
 class CategoriesController < ApplicationController
 
   def index
-    @categories = Category.all(limit: 2)
+    @categories = Category.all
   end
   def show
-    @category = Category.find_by_slug(params[:slug])
-    @category.nil? redirect_to(categories) : render :show
+    @category = Category.find_by_slug(params[:cat_slug])
+    @category.nil? ? redirect_to(categories_path) : render(:show)
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
   def self.find_by_slug(slug)
-    Category.find_by(title: slug)
+    Category.find_by(name: slug)
   end
 end
