3073b6fb77461681d3dd58fd11e7866c6fd5fd5a
Lrd134 <larryc3200@gmail.com>
2021-08-14 18:36:49 -0400

    added controller actions to fish

--- app/controllers/fish_controller.rb
+++ app/controllers/fish_controller.rb
@@ -1,8 +1,29 @@
 class FishController < ApplicationController
   before_action :set_fish, only: %i[ show edit update destroy ]
+  
   def index
     @fish = Fish.all
   end
+  
+  def new
+    @fish = Fish.new
+  end
+  
+  def create
+    byebug
+  end
+
+  def edit
+  end
+
+  def update
+    byebug
+  end
+  
+  def destroy
+    byebug
+  end
+  
   private
     
     def set_fish
@@ -10,6 +31,6 @@ class FishController < ApplicationController
     end
 
     def fish_params
-      params.fetch(:fish, {})
+      params.require(:fish).permit(:image, :content, :title, :user_id)
     end
 end
