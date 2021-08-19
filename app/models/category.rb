2865d196f3149d2537c86003d02746d32908c93a
Lrd134 <larryc3200@gmail.com>
2021-08-18 22:22:27 -0400

    added relationship to categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,7 @@
 class Category < ApplicationRecord
   has_many :fish
   has_many :users, through: :fish
+  has_many :comments, through: :fish
   def self.find_by_slug(slug)
     Category.find_by(name: slug)
   end
