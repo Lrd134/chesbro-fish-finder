4db0799d5033029577b6c82b06dc3e645631fff6
Lrd134 <larryc3200@gmail.com>
2021-08-18 20:25:17 -0400

    updated fish validations

--- app/models/fish.rb
+++ app/models/fish.rb
@@ -2,11 +2,9 @@ class Fish < ApplicationRecord
   has_many :comments
   belongs_to :category
   belongs_to :user
-  validates :title, exclusion: { in: %w( - ? _ + - . , ; : ' " [ ] { } \ | = ! @ # $ % ^ & * ),
-    message: "%{value} is reserved." }
-  after_validation :capitalize_title, :update_category
-  before_create :force_unsolved
-  validates :title, presence: true
+  validates :title, presence: true, exclusion: { in: %w( - ? _ + - . , ; : ' " [ ] { } \ | = ! @ # $ % ^ & * ),
+    message: "%{value} is reserved." }, length: { minimum: 3}
+  after_validation :capitalize_title
   has_one_attached :fish_image
 
   def slug
@@ -16,14 +14,7 @@ class Fish < ApplicationRecord
   def capitalize_title
     self.title = self.title.split(" ").map { | word | word.capitalize }.join(" ")
   end
-
-  def update_category
-    self.solved == 1 ? self.category=(Category.find_by_slug('identified')) : self.category=(Category.find_by_slug('unidentified'))
-  end
-  def force_unsolved
-    self.solved = 0
-    self.category = Category.find_by_slug('unidentified')
-  end
+  
   def self.deslugify(slug)
     slug.split("-").map { | word | word.capitalize }.join(" ")
   end
