5d2c50a4166f2714acdeae9895ec60819a931c5f
Lrd134 <larryc3200@gmail.com>
2021-08-20 06:11:41 -0400

    added newline after comment partial

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,7 +11,7 @@ class CommentsController < ApplicationController
     if current_user.id == params['comment']['user_id'].to_i && Fish.find_by_slug(params['fish_slug']).id == params['comment']['fish_id'].to_i
       @comment = Comment.create(comment_params)
       if @comment.nil?
-        redirect_to fish_path(@comment.fish_slug, @comment.category_slug), notice: "Comment was too short."
+        redirect_to fish_path(@comment.fish_slug, @comment.category_slug), notice: "Comment was too short, length must be greater than 15"
       else
         redirect_to fish_path(@comment.fish.slug, @comment.fish.category.slug), notice: "Comment successfully created."
       end
