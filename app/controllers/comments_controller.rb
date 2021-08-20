bd1c963878ecdfddd15e59e47a4d4de687cac1cd
Lrd134 <larryc3200@gmail.com>
2021-08-20 05:35:16 -0400

    added create action for comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,4 +7,17 @@ class CommentsController < ApplicationController
     @comment = Comment.find_by(id: params[:id])
     @comment.nil? ? redirect_to(comments_path) : render(:show)
   end
+  def create
+    byebug
+    if current_user.id == params['comment']['user_id'].to_i && Fish.find_by_slug(params['fish_slug']).id == params['comment']['fish_id']
+      @comment = Comment.create(comment_params)
+      redirect_to fish_path(@comment.fish.slug, @comment.fish.category.slug), notice: "Comment successfully created."
+    else
+      redirect_to fish_path(params['fish_slug'], params['cat_slug']), notice: "Error occurred"
+    end
+  end
+
+  def comment_params
+    params.require(:comment).permit(:user_id, :fish_id, :body)
+  end
 end
