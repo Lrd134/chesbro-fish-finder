3b10a02bba92e8ecbae7a47e20f41eeaace021e7
Lrd134 <larryc3200@gmail.com>
2021-08-20 06:13:13 -0400

    removed new route for comments

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
         get '/edit', to: 'fish#edit', as: 'edit_fish'
         get '/', to: 'fish#show', as: 'fish'
         post '/delete', to: 'fish#destroy', as: 'delete_fish'
-        resources :comments
+        resources :comments, except: [ :new ]
       end
     end
     get '/', to: 'fish#index', as: 'fish_index'
