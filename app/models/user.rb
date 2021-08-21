379f6fdcbca46ed488efab1f47cd9b5aa1231b76
Lrd134 <larryc3200@gmail.com>
2021-08-21 14:01:05 -0400

    removed param_check from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,14 +31,6 @@ class User < ApplicationRecord
   def set_admin
     self.admin.nil? ? self.admin=(0) : self.admin=(self.admin)
   end
-  
-  def param_check?(params, model)
-    if params[model]['user_id'].to_i == self.id
-      true
-    else
-      false
-    end
-  end
 
   def admin?
     !self.admin
