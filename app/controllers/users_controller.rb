348dcd0a6a186136d79d5dc245284dd3f0b97834
Lrd134 <larryc3200@gmail.com>
2021-08-21 01:45:09 -0400

    fixed users#show and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,18 @@
 class UsersController < ApplicationController
-  before_action :current_user, only: %i[ show edit update destroy ]
+  before_action :current_user, only: %i[ edit update destroy ]
 
   def index
     @users = User.all
   end
 
   def show
+    @current_user = current_user
+    @user = User.find_by(id: params['id'])
+    if !@user.nil?
+      render :show
+    else
+      redirect_to users_path, notice: "User not found."
+    end
     
   end
 
