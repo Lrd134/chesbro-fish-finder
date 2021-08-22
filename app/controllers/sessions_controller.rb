6c6df959b62ff3353f97b418688a06747e8d9a7d
Lrd134 <larryc3200@gmail.com>
2021-08-22 16:52:55 -0400

    added username check to oauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,12 @@
 class SessionsController < ApplicationController
   def omniauth
-    user = User.from_omniauth(request.env['omniauth.auth'])
-    session[:uid] = user.id
-    redirect_to user_path(user)
+    @user = User.from_omniauth(request.env['omniauth.auth'])
+    session[:uid] = @user.id
+    if @user.username.nil?
+      redirect_to create_username_path(@user)
+    else
+      redirect_to user_path(@user)
+    end
   end
   def login
     @user = User.new
