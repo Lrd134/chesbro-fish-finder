00e51d1d79246498cac7930e95dbcd44ed7ade26
Lrd134 <larryc3200@gmail.com>
2021-08-23 23:06:39 -0400

    fixed nil values for user data

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,9 +2,11 @@ class Category < ApplicationRecord
   has_many :fish
   has_many :users, through: :fish
   has_many :comments, through: :fish
+  
   def self.find_by_slug(slug)
     Category.find_by(name: slug)
   end
+
   def slug
     self.name
   end
